472f6662d448f42a860b21b3bcb6a730c084a5e0
Tyler Mayoff <tyler@tylermayoff.com>
2023-12-17 21:00:12 -0500

    updated flake

--- flake.nix
+++ flake.nix
@@ -2,23 +2,32 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:lopsided98/nixpkgs/nix-ros";
     flake-utils.url = "github:numtide/flake-utils";
-    ros-overlay.url = "github:lopsided98/nix-ros-overlay/e3eba7bb67e60108ba22ce297d72e1a9ad71311f";
+    ros-overlay.url = "github:lopsided98/nix-ros-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils, ros-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system; 
-	overlays = [ ros-overlay.overlays.default ];
-      }; in {
-
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ros-overlay.overlays.default ];
+        };
+      in
+      {
         devShell = pkgs.mkShell {
-	  buildInputs = with pkgs; with rosPackages.humble; [
-	    cmake
-	    ros-core
-	  ];
+          nativeBuildInputs = with pkgs; with pkgs.rosPackages.humble; [
+            (buildEnv {
+              paths = [
+                ament-cmake-core
+		python-cmake-module
+                ros-core
+                vcstool
+                colcon
+              ];
+            })
+          ];
         };
       });
 }
